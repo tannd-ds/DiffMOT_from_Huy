c0d0521598e0e0f7982a4a127be6d4218ac36621
LeHuyTu <101401285+21522173huy@users.noreply.github.com>
2024-11-19 17:32:58 +0700

    Update main.py

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+
 from diffmot import DiffMOT
 import argparse
 import yaml
@@ -6,10 +7,11 @@ from easydict import EasyDict
 def parse_args():
     parser = argparse.ArgumentParser(
         description='Pytorch implementation of MID')
-    parser.add_argument('--config', default='')
-    parser.add_argument('--dataset', default='')
+    parser.add_argument('--config', default='', help='Path to the config file')
+    parser.add_argument('--dataset', default='', help='Dataset name')
+    parser.add_argument('--data_dir', default=None, help='Path to the data directory')
+    parser.add_argument('--epochs', type=int, default=None, help='Number of epochs')
     return parser.parse_args()
-
 
 def main():
     args = parse_args()
@@ -17,17 +19,24 @@ def main():
        config = yaml.safe_load(f)
 
     for k, v in vars(args).items():
-       config[k] = v
+       if v is not None:
+           config[k] = v
     config["exp_name"] = args.config.split("/")[-1].split(".")[0]
     config["dataset"] = args.dataset
     config = EasyDict(config)
+    
+    # Update config with command-line arguments if provided
+    if args.data_dir is not None:
+        config.data_dir = args.data_dir
+    if args.epochs is not None:
+        config.epochs = args.epochs
+
     agent = DiffMOT(config)
 
-    if config["eval_mode"]:
+    if config.eval_mode:
         agent.eval()
     else:
         agent.train()
-
 
 if __name__ == '__main__':
     main()
